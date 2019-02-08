552cfeea891dac96acb1553c025ed8cd2c2dfdfa
5412jason <jasonhogan5412@gmail.com>
2019-02-07 19:49:41 -0500

    Completed initial application.
    Began testing procedures.

--- Program_1/code/main.cpp
+++ Program_1/code/main.cpp
@@ -50,7 +50,7 @@ public:
 			node* temp = head;
 			string value = head->value;
 			head = head->next;
-			//delete(temp);
+			delete(temp);
 			return value;
 		}
 	}
@@ -61,6 +61,15 @@ public:
 		}
 		else{
 			return false;
+		}
+	}
+
+	string top() {
+		if(isEmpty()) {
+			return "";
+		}
+		else {
+			return head->value;
 		}
 	}
 };
@@ -111,9 +120,9 @@ public:
 	T pop() {
 		T value = topNode->value;
 		if (!isEmpty()) {
-			//node* temp = topNode;
+			node* temp = topNode;
 			topNode = topNode->next;
-			//delete(temp);
+			delete(temp);
 		}
 		return value;
 	}
@@ -125,11 +134,15 @@ public:
 
 void log(const std::string &input){
 	ofstream filestream(FILENAME, std::ios_base::app|std::ios_base::out);
+	filestream.flush();
+	cout.flush();
 	std::cout << input << endl;
 	filestream << input << endl;
+	filestream.close();
+
 }
 
-bool isOperand(char input){
+bool isOperator(char input){
 	if (input == '+' || input == '-' || input == '/' || input == '*'){
 		return true;
 	}
@@ -138,7 +151,7 @@ bool isOperand(char input){
 	}
 }
 
-bool isOperator(char input){
+bool isOperand(char input){
 	if(input >= 'A' && input <= 'Z'){
 		return true;
 	}
@@ -160,59 +173,81 @@ int getOperatorWeight(char input){
 	}
 }
 
-string infixToPostfix(std::string infix){
+treeNode* convertPostfixToTree(string postfix) {
+	tStack<treeNode*> treeStack;
+	treeNode* root = new treeNode();
+	for (int i = 0; i < postfix.size(); i++) {
+		treeNode* node = new treeNode();
+		node->value = postfix[i];
+		if (isOperand(postfix[i])) {
+			treeStack.push(node);
+		}
+		if (isOperator(postfix[i])) {
+			treeNode* temp = treeStack.pop();
+			node->right = temp;
+			temp = treeStack.pop();
+			node->left = temp;
+			treeStack.push(node);
+		}
+	}
+	root = treeStack.pop();
+	return root;
+}
+
+string infixToPostfix(string infix){
 	std::string postfix = "";
 	tStack<char> stack;
 	int parCount = 0;
 	int prevIndex = 0;
 	bool errors = false;
 
-	char characters[infix.size()];
-	strcpy (characters, infix.c_str());
-
 	for (int i = 0; i <= infix.size(); i++){
-		if(characters[i] == ' '){
+		if(infix[i] == ' '){
 			i += 1;
-			if (i > infix.length()){
-				if(postfix.size() != 0){
-					// string is done being parsed
-					break;
+			if (i > infix.length() - 1 ){
+				if (isOperator(infix[prevIndex])) {
+					errors = true;
+					log("[ERROR] Missing second operand at indexes: " + to_string(prevIndex) + ',' + to_string(i));
 				}
-				else{
+				else if (postfix.size() == 0){
 					errors = true;
 					log("[ERROR] Empty line!");
 				}
+				else {
+					// all done
+					break;
+				}
 			}
-			else if (isOperand(characters[prevIndex] && isOperand(characters[i]))){
+			else if (isOperand(infix[prevIndex]) && isOperand(infix[i])){
 				errors = true;
-				log("[ERROR] Two operands cannot be next to each other! At indexes: " + characters[prevIndex] + ',' + characters[i]);
+				log("[ERROR] Two operands cannot be next to each other! At indexes: " + to_string(prevIndex) + ',' + to_string(i));
 			}
-			else if (isOperator(characters[prevIndex]) && characters[i] == '('){
+			else if (isOperator(infix[prevIndex]) && infix[i] == '('){
 				errors = true;
-				log("[ERROR] Missing operand! At indexes: " + characters[prevIndex] + ',' + characters[i]);
+				log("[ERROR] Missing operand! At indexes: " + to_string(prevIndex) + ',' + to_string(i));
 			}
-			else if (characters[prevIndex] == ')' && characters[i] == '('){
+			else if (infix[prevIndex] == ')' && infix[i] == '('){
 				errors = true;
-				log("[ERROR] Missing operator! At indexes: " + characters[prevIndex] + ',' + characters[i]);
+				log("[ERROR] Missing operator! At indexes: " + to_string(prevIndex) + ',' + to_string(i));
 			}
-			else if (characters[i] == ' '){
+			else if (infix[i] == ' '){
 				errors = true;
-				log("[ERROR] Invalid spacing! At indexes: " + characters[prevIndex] + ',' + characters[i]);
+				log("[ERROR] Invalid spacing! At indexes: " + to_string(prevIndex) + ',' + to_string(i));
 			}
-			else if (isOperand(characters[i])){
-				postfix += characters[i];
+			else if (isOperand(infix[i])){
+				postfix += infix[i];
 			}
-			else if (isOperator(characters[i])){
-				while(stack.isEmpty() == false && stack.top() != '(' && getOperatorWeight(stack.top() > getOperatorWeight(characters[i]))){
+			else if (isOperator(infix[i])){
+				while(stack.isEmpty() == false && stack.top() != '(' && getOperatorWeight(stack.top()) >= getOperatorWeight(infix[i])){
 					postfix += stack.pop();
 				}
-				stack.push(characters[i]);
+				stack.push(infix[i]);
 			}
-			else if(characters[i] == '('){
-				stack.push(characters[i]);
+			else if(infix[i] == '('){
+				stack.push(infix[i]);
 				parCount += 1;
 			}
-			else if (characters[i] == ')'){
+			else if (infix[i] == ')'){
 				while(stack.isEmpty() == false && stack.top() != '('){
 					postfix += stack.pop();
 				}
@@ -222,10 +257,11 @@ string infixToPostfix(std::string infix){
 				parCount -= 1;
 			}
 		}
-		else if (characters[i] != ' '){
+		else if (infix[i] != ' '){
 			errors = true;
-			log("[ERROR] Invalid spacing! At indexes: " + characters[prevIndex] + '+' + characters[i]);
+			log("[ERROR] Invalid spacing! At indexes: " + to_string(prevIndex) + ',' + to_string(i));
 		}
+		prevIndex = i;
 	}
 	while (stack.isEmpty() == false){
 		postfix +=stack.pop();
@@ -242,8 +278,163 @@ string infixToPostfix(std::string infix){
 	}
 }
 
+string getPostfixOperations(string postfix) {
+	string operations;
+	string operation1;
+	string operation2;
+	tStack<string> stack;
+	for (int i = 0; i < postfix.size(); i++) {
+		if (isOperand(postfix[i])) {
+			stack.push(string(1, postfix[i]));
+		}
+		else if (isOperator(postfix[i])) {
+			operation1 = stack.pop();
+			operation2 = stack.pop();
+			string temp = operation2 + operation1 + postfix[i];
+			stack.push(temp);
+			operations += temp + "\n";
+		}
+	}
+	return operations;
+}
+
+string visualizeTree(treeNode* root, int spacing) {
+	string temp;
+	if (root != NULL) {
+		spacing += 5;
+		temp += visualizeTree(root->right, spacing);
+		temp += "\n";
+		for (int i = 0; i < spacing; i++) {
+			temp += " ";
+		}
+		temp += string(1,root->value);
+		temp += visualizeTree(root->left, spacing);
+
+	}
+	return temp;
+}
+
+string getPostfixFromTree(treeNode* root) {
+	string postfix;
+	if (root == NULL) {
+		return "";
+	}
+	else {
+		if (root->left != NULL) {
+			postfix += getPostfixFromTree(root->left);
+		}
+		if (root->right != NULL) {
+			postfix += getPostfixFromTree(root->right);
+		}
+		postfix += string(1,root->value);
+	}
+	return postfix;
+}
+string getPrefixFromTree(treeNode* root) {
+	string prefix;
+	if (root == NULL) {
+		return "";
+	}
+	else {
+		prefix += string(1, root->value);
+		if (root->left != NULL) {
+			prefix += getPrefixFromTree(root->left);
+		}
+		if (root->right != NULL) {
+			prefix += getPrefixFromTree(root->right);
+		}
+	}
+	return prefix;
+}
+string getInfixFromTree(treeNode* root) {
+	string infix;
+	if (root == NULL) {
+		return "";
+	}
+	else {
+		if (root->left != NULL) {
+			infix += getInfixFromTree(root->left);
+		}
+		infix += root->value;
+		if (root->right != NULL) {
+			infix += getInfixFromTree(root->right);
+		}
+	}
+	return infix;
+}
+
+int getVariableValue(char input) {
+	if (input >= 'A' && input <= 'Z') {
+		return input - 64;
+	}
+	else {
+		log("[ERROR][getVariableValue] Invalid Character, defaulting to -1: " + string(1, input));
+		return -1;
+	}
+}
+
+void solutionFromPrefix(string prefix) {
+	tStack<string> opStack;
+	tStack<int> intStack;
+	int curVal = 0;
+	for (int i = prefix.size() - 1; i >= 0; i--) {
+
+		if (isOperand(prefix[i])) {
+			curVal = getVariableValue(prefix[i]);
+			opStack.push(string(1,prefix[i]));
+			intStack.push(getVariableValue(prefix[i]));
+		}
+		else {
+			if (opStack.isEmpty() == false) {
+				string temp = string(1,prefix[i]);
+				int tmpInt;
+				string str1, str2;
+				str1 = opStack.pop();
+				temp += str1;
+				str2 = opStack.pop();
+				temp += str2;
+				opStack.push(temp);
+
+				if (prefix[i] == '+') {
+					int int1 = intStack.pop();
+					int int2 = intStack.pop();
+					int tmp = int1 + int2;
+					intStack.push(tmp);
+				}
+				else if (prefix[i] == '-') {
+					int int1 = intStack.pop();
+					int int2 = intStack.pop();
+					int tmp = int1 - int2;
+					intStack.push(tmp);
+				}
+				else if (prefix[i] == '*') {
+					int int1 = intStack.pop();
+					int int2 = intStack.pop();
+					int tmp = int1 * int2;
+					intStack.push(tmp);
+				}
+				else if (prefix[i] == '/') {
+					int int1 = intStack.pop();
+					int int2 = intStack.pop();
+					if (int2 != 0) {
+						int tmp = int1 / int2;
+						intStack.push(tmp);
+					}
+					else {
+						log("[ERROR] Cannot divide by zero!");
+						return;
+					}
+				}
+				curVal = intStack.top();
+				log(temp + "   =   " + to_string(intStack.top()));
+			}
+		}
+	}
+	log("Final Solution: " + to_string(curVal));
+}
+
 // main method for testing stuff
-int main(int argc, const char* argv[]) {
+/*int main(int argc, const char* argv[]) {
 	log("Welcome to the program!");
 	//logger->log("Logging test ");
 	tStack<char> test;
@@ -254,8 +445,56 @@ int main(int argc, const char* argv[]) {
 	test.push('c');
 	log("push: c");
 	while (!test.isEmpty()) {
-		log("pop: " + test.top());
-		test.pop();
+		log("pop: " + string(1,test.pop()));
+		//test.pop();
 	}
+	system("PAUSE");
+	return 1;
+}*/
+
+int main() {
+	log("\nWelcome to the best program ever!");
+	log("Please enter an input file name: ");
+	string inputFileName;
+	getline(cin, inputFileName);
+	ifstream inFile(inputFileName);
+	if (!inFile) {
+		log(inputFileName + " Does not exist!");
+		system("PAUSE");
+		return 1;
+	}
+	else if (inFile.peek() == ifstream::traits_type::eof()) {
+		log(inputFileName + " File is empty!");
+		system("PAUSE");
+		return 1;
+	}
+	log(inputFileName + " File was opened successfully!");
+
+	string line;
+	while (getline(inFile, line)) {
+		log("===============================================");
+		log("Input: " + line);
+		string postfix;
+		postfix = infixToPostfix(line);
+		if (postfix != "") {
+			log("\nPostfix: " + postfix);
+			string operations = getPostfixOperations(postfix);
+			log("\nOperations: " + operations);
+			treeNode* root = convertPostfixToTree(postfix);
+			string visTree = visualizeTree(root, 1);
+			log("\nTree: \n" + visTree);
+			string postfixTree = getPostfixFromTree(root);
+			string infixTree = getInfixFromTree(root);
+			string prefixTree = getPrefixFromTree(root);
+			log("\nPostfix from tree: " + postfixTree);
+			log("Prefix from tree: " + prefixTree);
+			log("Infix from tree: " + infixTree);
+			solutionFromPrefix(prefixTree);
+		}
+	}
+
+	log("Thank you for using the program! Have a great day!");
+
+	system("PAUSE");
 	return 1;
 }
